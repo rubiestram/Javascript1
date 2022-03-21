75580ea54f7d0ba368c8b9dd003eddd7beb64466
rubiestram <rubiestram@gmail.com>
2022-03-21 13:22:09 -0600

    Agregue actividad JS1

---
+++ javascript1.js
@@ -0,0 +1,14 @@
+//Función
+function sumar (x,y){
+    return x + y;
+}
+
+//Solicitamos al usuario los valores
+const numero1 = parseInt(prompt("Introduce un número: "));
+const numero2 = parseInt(prompt("Introduce otro número: "));
+
+const resultado = sumar(numero1, numero2);
+console.log(resultado);
+
+
+
